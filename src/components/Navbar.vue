652acb8b57c8bac4707419a7c589315822b5bc81
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-31 15:21:47 +0300

    add navigation menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,33 @@
 <template>
     <div>
-        <v-navigation-drawer app temporary v-model="sideNav"></v-navigation-drawer>
+        <v-navigation-drawer app temporary v-model="sideNav">
+            <v-list>
+                <v-list-tile
+                    v-for="(link,i) in links"
+                    :key="i"
+                    :to="link.url"
+                    >
+                    <v-list-tile-action>
+                        <v-icon>{{link.icon}}</v-icon>
+                    </v-list-tile-action>
+
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="link.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
         <v-toolbar app dark color="primary">
             <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
             <v-toolbar-title>Online Store</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn flat>
-                    <v-icon left>account_box</v-icon>
-                    Link One
+                <v-btn flat
+                       v-for="(link,i) in links"
+                       :key="i"
+                       :to="link.url">
+                    <v-icon left>{{link.icon}}</v-icon>
+                    {{link.title}}
                 </v-btn>
             </v-toolbar-items>
         </v-toolbar>
@@ -22,7 +41,14 @@
 export default{
     data () {
         return {
-            sideNav: false
+            sideNav: false,
+            links:[
+                {title:'Login',icon:'account_box',url:'/login'},
+                {title:'Register',icon:'face',url:'/register'},
+                {title:'Cart',icon:'shopping_cart',url:'/checkout'},
+                {title:'New Product',icon:'add',url:'/new'},
+                {title:'My Products',icon:'account_box',url:'/login'},
+                ]
         }
     }
 }
