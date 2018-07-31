204258fb86a53d7c497e3aed90fd3718d281e04a
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-31 18:45:48 +0300

    add page orders

--- src/components/User/Checkout.vue
+++ src/components/User/Checkout.vue
@@ -1,8 +1,36 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Checkout</h1>
+            <v-flex xs12 sm6 offset-sm3>
+            <h1 class="text-secondary mb3">Orders</h1>
+            <v-list subheader two-line>
+
+                      <v-list-tile
+                      avatar
+                      v-for="(order,i) in orders"
+                      :key="i"
+                      >
+                        <v-list-tile-action>
+                          <v-checkbox
+                            color="success"
+                            :input-value="order.done"
+                            @change="markDone(order)"
+                          ></v-checkbox>
+                        </v-list-tile-action>
+
+                        <v-list-tile-content>
+                          <v-list-tile-title>{{order.name}}</v-list-tile-title>
+                          <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn
+                                :to="'/product/' + order.productId"
+                            class="primary"
+                            >Open</v-btn>
+                        </v-list-tile-action>
+                      </v-list-tile>
+
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -12,7 +40,21 @@
     export default{
         data(){
             return{
+                orders:[
+                    {
+                        id:'test',
+                        name:'Antony',
+                        phone:'22-22-22',
+                        poductId:'1',
+                        done:false
 
+                    }
+                ]
+            }
+        },
+        methods:{
+            markDone(order){
+                order.done = true
             }
         }
     }
