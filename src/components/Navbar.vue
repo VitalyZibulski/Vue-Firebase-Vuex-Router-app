8e9472611a3d883dab9d5b62378a0537a3667559
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-31 15:00:21 +0300

    add navigation drawyer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        <v-navigation-drawer app temporary></v-navigation-drawer>
+        <v-navigation-drawer app temporary v-model="sideNav"></v-navigation-drawer>
         <v-toolbar app dark color="primary">
-            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
             <v-toolbar-title>Online Store</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn flat>Link One</v-btn>
+                <v-btn flat>
+                    <v-icon left>account_box</v-icon>
+                    Link One
+                </v-btn>
             </v-toolbar-items>
         </v-toolbar>
         <v-content>
@@ -16,6 +19,11 @@
 </template>
 
 <script>
-    export default{
+export default{
+    data () {
+        return {
+            sideNav: false
+        }
     }
+}
 </script>
